a07077336533d8a32d2c1e73f29725777c92c362
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-22 17:03:41 +0200

    Backup

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const box = document.querySelector('.box');
 const playButton = document.getElementById('start-game');
-let BOMBPOSITION = [];
+const bombPositions = [];
+let score = document.getElementById('score-number');
+console.log(score);
+let scorePoint= 0;
+score.innerText = scorePoint;
+
 
 function selectDifficulty(){
   let difficulty = document.getElementById('difficulty').value;
@@ -19,6 +24,8 @@ function squareGenerator(n){
     square.classList.add('square');
     const number = document.createElement('span');
     number.classList.add('d-none');
+    number.innerText = `${(i + 1)}`;
+    square.append(number);
 
     if(n == 49){
       square.classList.add('square-49');
@@ -29,26 +36,50 @@ function squareGenerator(n){
     }
 
     box.append(square);
-    number.innerText = `${(i + 1)}`;
-    square.append(number);
+
     square.addEventListener('click', function(){
       
       number.classList.remove('d-none');
-      if(BOMBPOSITION.includes(parseInt(this.innerText))){
+      if(bombPositions.includes(parseInt(this.innerText))){
         this.classList.add('bomb');
         this.innerHTML = '<i class="fa-solid fa-bomb"></i>';
-      }else this.classList.add('blue');
+        const allSquare = document.querySelectorAll('.d-none');
+        console.log(allSquare);
+        for (let i = 0; i < allSquare.length; i++) {
+          const actualPos = allSquare[i].innerText;
+          console.log(actualPos);
+          for (let k = 0; k < bombPositions.length; k++) {
+            if (actualPos == bombPositions[k]) {
+              console.log(allSquare[i].outerHTML);
+              allSquare[i].classList.add('bomb');
+              allSquare[i].classList.remove('d-none');
+              allSquare[i].innerHTML = '<i class="fa-solid fa-bomb"></i>';
+            }
+            
+          }
+          
+        }
+        
+      }else if((!this.className.includes('blue')) && (!this.className.includes('bomb'))){
+        this.classList.add('blue');
+        scorePoint++;
+        score.innerText = scorePoint;
+      }
+
     })
   }
 }
 // Inizia il gioco
 playButton.addEventListener('click', function(){
+  
   box.innerHTML = '';
+  scorePoint = 0;
+  score.innerText = scorePoint;
   const diff = selectDifficulty();
   squareGenerator(diff);
-  BOMBPOSITION = [];
+  bombPositions.length = 0;
   bombGenerator(diff);
-  console.log(BOMBPOSITION)
+  console.log(bombPositions)
 })
 
 function getRndInteger(min, max){
@@ -58,11 +89,15 @@ function getRndInteger(min, max){
 function bombGenerator(n){
   const bombNumber = 16;
 
-  while(BOMBPOSITION.length < bombNumber){
+  while(bombPositions.length < bombNumber){
     let posBomb = getRndInteger(1,n);
 
-    if(!BOMBPOSITION.includes(posBomb)){
-      BOMBPOSITION.push(posBomb);
+    if(!bombPositions.includes(posBomb)){
+      bombPositions.push(posBomb);
     }
   }
+}
+
+function endGame(){
+
 }
